28f6ebddd1f93de364b546490e3214786a816a1d
Moritz Rebbert <moritz.rebbert@udo.edu>
2015-12-08 22:37:09 +0100

    docs updated #2

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Box",
+        "Garfunkel",
+        "Pool",
+        "Segment",
+        "Vect"
+    ],
+    "modules": [
+        "Garfunkel"
+    ],
+    "allModules": [
+        {
+            "displayName": "Garfunkel",
+            "name": "Garfunkel",
+            "description": "garfunkel.js - A 2D geometry toolbox"
+        }
+    ],
+    "elements": []
+} };
+});
