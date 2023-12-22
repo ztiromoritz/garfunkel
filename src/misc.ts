eaa9bed8e2ceeff50092f97e250fd429c7fe6c7b
Moritz Rebbert <moritz.rebbert@udo.edu>
2023-12-23 00:36:55 +0100

    circle point refined

--- src/misc.ts
+++ src/misc.ts
@@ -2,55 +2,74 @@ import { Circle } from './circle';
 import { wrap } from './pool';
 import { _s, intersectLines } from './segment-functions';
 import { Vect } from './vect';
-import {
-  _v,
-  distance,
-  length as vect_length,
-} from './vect-functions';
+import { _v, distance, length as vect_length } from './vect-functions';
+
+type Intersect = {
+	type: 'INTERSECT';
+	t: number;
+};
+
+type FlyBy = {
+	type: 'FLY_BY';
+	t: number;
+};
+
+export type IntersectCirclePointResult = Intersect | FlyBy;
 
 /**
  * Collide a moving circle with a given point u.
  **/
 export function circle_point_intersect(
-  circle: Circle,
-  movement: Vect,
-  u: Vect,
-) {
-  return wrap(() => {
-    const m = _v(circle.x, circle.y);
+	circle: Circle,
+	movement: Vect,
+	u: Vect,
+): IntersectCirclePointResult {
+	return wrap<IntersectCirclePointResult>(() => {
+		const m = _v(circle.x, circle.y);
 
 		// Take s1 as the movement segment of the center
-    const s1 = _s.fromSupport(m, movement);
+		const s1 = _s.fromSupport(m, movement);
 
 		// Find a line through u ortogonal to s1
-    const dir = movement.clone().toRightNormal();
-    const s2 = _s.fromSupport(u, dir);
+		const dir = movement.clone().toRightNormal();
+		const s2 = _s.fromSupport(u, dir);
 
 		// intersect them
-    const intersect = intersectLines(s1, s2);
-    if (intersect.type === 'INTERSECT') {
-			const p = s1.getPoint(intersect.t1)
+		const intersect = intersectLines(s1, s2);
+		if (intersect.type === 'INTERSECT') {
+			const p = s1.getPoint(intersect.t1);
+
+			const a = distance(u, p);
+			if (a > circle.radius) {
+				return {
+					type: 'FLY_BY',
+					t: intersect.t1,
+				};
+			}
 
 			// At the time of collision the three points:
 			// u - The target point
 			// p - the point where s1 and s2 intersect
 			// m - the center of the circle
 			// create a right-angled triangle with r as hypothenuse (u,m).
+			const c = circle.radius;
+			const b = Math.sqrt(c * c - a * a);
 
-      // Pythagoras
-      const a = distance(u, p);
-      const c = circle.radius;
-      const b = Math.sqrt(c * c - a * a);
-			
-			// Distance the circle already moved 
+			// Distance the circle already moved
 			// as percentage of the whole movement
-      return (distance(m, p) - b) / vect_length(movement);
-    } else {
+			return {
+				type: 'INTERSECT',
+				t: (distance(m, p) - b) / vect_length(movement),
+			};
+		} else {
 			// TODO:
 			// Can we get this case first without intersect
 			// Line-vs-Point intersection
-      const b = circle.radius;
-      return distance(m, u) - b / vect_length(movement);
-    }
-  });
+			const b = circle.radius;
+			return {
+				type: 'INTERSECT',
+				t: distance(m, u) - b / vect_length(movement)
+			}
+		}
+	});
 }
--- src/test/misc.test.ts
+++ src/test/misc.test.ts
@@ -4,18 +4,38 @@ import { _v } from '../vect-functions';
 import { _c } from '../circle-functions';
 
 describe('Misc', () => {
-	describe('segment-function', () => {
-		/**
-		 * <img src="media://">
-		 **/
-		it('circle_point_intersect', () => {
-			const result = circle_point_intersect(_c(1, -4, 2), _v(-3, 6), _v(0, 0));
-			assert(Math.abs(result - 0.333333) < 0.001);
-		});
+  describe('segment-function', () => {
+    /**
+     * <img src="media://">
+     **/
+    it('circle_point_intersect', () => {
+      const result = circle_point_intersect(_c(1, -4, 2), _v(-3, 6), _v(0, 0));
+      assert(result.type === 'INTERSECT');
+      assert(Math.abs(result.t - 0.333333) < 0.001);
+    });
 
-		it('circle_point_intersect_point_on_movement', () => {
-			const result = circle_point_intersect(_c(0, 0, 2), _v(6, 0), _v(4, 0));
-			assert(Math.abs(result - 0.333333) < 0.001);
-		});
-	});
+    it('circle_point_intersect_point_on_movement', () => {
+      const result = circle_point_intersect(_c(0, 0, 2), _v(6, 0), _v(4, 0));
+      assert(result.type=== 'INTERSECT');
+      assert(Math.abs(result.t - 0.333333) < 0.001);
+    });
+
+    it('circle_point_intersect_point_tangent', () => {
+      const result = circle_point_intersect(_c(0, 0, 2), _v(6, 0), _v(2, 2));
+			assert(result.type === 'INTERSECT')
+      assert(Math.abs(result.t - 0.333333) < 0.001);
+    });
+
+    it('circle_point_intersect_point_fly_by_not_yet', () => {
+      const result = circle_point_intersect(_c(0, 0, 2), _v(6, 0), _v(12, 3));
+			assert(result.type == 'FLY_BY')
+      assert(Math.abs(result.t - 2) < 0.001);
+    });
+
+    it('circle_point_intersect_point_miss', () => {
+      const result = circle_point_intersect(_c(0, 0, 2), _v(6, 0), _v(4, 3));
+			assert(result.type == 'FLY_BY')
+      assert(Math.abs(result.t - 0.66666666) < 0.001);
+    });
+  });
 });
