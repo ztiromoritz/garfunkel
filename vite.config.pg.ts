5503924f4ba30844cdbb1279a106267443977fa4
Moritz Rebbert <moritz.rebbert@udo.edu>
2024-01-27 23:58:51 +0100

    build process as _site build

---
+++ vite.config.pg.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
+//import DynamicPublicDirectory from 'vite-multiple-assets';
+
+export default defineConfig({
+	plugins: [
+//		DynamicPublicDirectory(['./public', './docs/'])
+	],
+	server: {
+		port: 5174
+	},
+	build: {
+		outDir: "./dist-pg/",
+		rollupOptions: {
+			input: {
+				playground: path.resolve(__dirname, 'playground.html'),
+			},
+		},
+	},
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import dts from 'vite-plugin-dts';
-import DynamicPublicDirectory from 'vite-multiple-assets';
+//import DynamicPublicDirectory from 'vite-multiple-assets';
+//
 
 export default defineConfig({
-	plugins: [dts(), DynamicPublicDirectory(['./public', './docs/'])],
+	plugins: [
+		dts({
+			outDir: 'dist/types',
+			include: ['src'],
+			exclude: ['src/repl', 'src/test']
+			//rollupTypes: true,
+		}),
+		//		DynamicPublicDirectory(['./public', './docs/'])
+	],
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/main.ts'),
@@ -13,16 +22,22 @@ export default defineConfig({
 		},
 		rollupOptions: {
 			input: {
-				main: path.resolve(__dirname, 'index.html'),
-				playground: path.resolve(__dirname, 'playground.html'),
+				garfunkel: path.resolve(__dirname, 'src/main.ts'),
 			},
-			output: {
-				format: 'esm',
-				inlineDynamicImports: false,
-			},
+			output: [
+				{
+					format: 'es',
+					entryFileNames: '[name].[format].js',
+				},
+				{
+					format: 'umd',
+					name: 'Garfunkel',
+					entryFileNames: '[name].[format].js',
+				},
+			],
 		},
 	},
 	test: {
 		exclude: ['ideas/**/*', '**/node_modules/**', '**/dist/**'],
 	},
-})false
+});
